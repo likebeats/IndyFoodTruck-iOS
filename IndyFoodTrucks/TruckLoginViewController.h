173189e0b493b3c4645ead948e2e9ca8bd438613
SJ Singh <sj@appdar.com>
2014-04-04 20:34:59 -0400

    Add map view. Added TruckLoginViewController. Temp button.

---
+++ IndyFoodTrucks/TruckLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  TruckLoginViewController.h
+//  IndyFoodTrucks
+//
+//  Created by sj singh on 4/4/14.
+//  Copyright (c) 2014 AppDar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TruckLoginViewController : UIViewController
+
+@end
